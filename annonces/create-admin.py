10d6bf169ed85e42bddbb78aa03ac4eed1bbc0a1
Immopros <tip@immopros.fr>
2026-02-10 06:56:30 +0100

    Add function to create superuser if none exists

--- annonces/create-admin.py
+++ annonces/create-admin.py
@@ -1,1 +1,13 @@
+from django.contrib.auth import get_user_model
+from django.conf import settings
 
+def create_superuser():
+    User = get_user_model()
+
+    if not User.objects.filter(is_superuser=True).exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@annoncesgratos.fr",
+            password="Admin12345!"
+        )
+        print("✅ Superuser créé automatiquement")
