d66db52b2b64eaa2195391e8a2c1a0b825138037
Immopros <tip@immopros.fr>
2026-02-10 12:23:39 +0100

    Update apps.py

--- annonces/apps.py
+++ annonces/apps.py
@@ -4,6 +4,6 @@ class AnnoncesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'annonces'
 
-    def ready(self):
+    
         from .create_admin import create_superuser
         create_superuser()
