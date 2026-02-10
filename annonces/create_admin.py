0fa42d4e79116e4070eb3b1383ce9712f0db76fd
Immopros <tip@immopros.fr>
2026-02-10 14:04:23 +0100

    Update create_admin.py

--- annonces/create_admin.py
+++ annonces/create_admin.py
@@ -1,16 +1,5 @@
-import os
-from django.contrib.auth import get_user_model
+from django.apps import AppConfig
 
-def create_superuser():
-    username = os.environ.get("SUPERUSER_USERNAME")
-    email = os.environ.get("SUPERUSER_EMAIL", "")
-    password = os.environ.get("SUPERUSER_PASSWORD")
-
-    if not username or not password:
-        return
-
-    User = get_user_model()
-    if User.objects.filter(username=username).exists():
-        return
-
-    User.objects.create_superuser(username=username, email=email, password=password)
+class AnnoncesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'annonces'
