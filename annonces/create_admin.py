e9612ab85f27236daa5fbcc950447c5a7a3b4036
Immopros <tip@immopros.fr>
2026-02-10 07:24:58 +0100

    Refactor create_superuser to use env variables
    Update superuser creation to use environment variables for username, email, and password.

--- annonces/create_admin.py
+++ annonces/create_admin.py
@@ -1,13 +1,16 @@
+import os
 from django.contrib.auth import get_user_model
-from django.conf import settings
 
 def create_superuser():
-    User = get_user_model()
+    username = os.environ.get("SUPERUSER_USERNAME")
+    email = os.environ.get("SUPERUSER_EMAIL", "")
+    password = os.environ.get("SUPERUSER_PASSWORD")
 
-    if not User.objects.filter(is_superuser=True).exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="admin@annoncesgratos.fr",
-            password="Admin12345!"
-        )
-        print("✅ Superuser créé automatiquement")
+    if not username or not password:
+        return
+
+    User = get_user_model()
+    if User.objects.filter(username=username).exists():
+        return
+
+    User.objects.create_superuser(username=username, email=email, password=password)
