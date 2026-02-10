8d7666027af98b9121eafb506cb36041c5d699d5
Immopros <tip@immopros.fr>
2026-02-10 06:55:46 +0100

    Implement ready method to create superuser
    Add a ready method to create a superuser on app startup.

--- annonces/apps.py
+++ annonces/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class AnnoncesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'annonces'
+
+    def ready(self):
+        from .create_admin import create_superuser
+        create_superuser()
