ad0544d45f7d99bc443130c8d175e89c0ea3e732
Immopros <tip@immopros.fr>
2026-02-10 11:54:40 +0100

    Add setup-admin path to urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from annonces.views import setup_admin
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("setup-admin/<str:token>/", setup_admin),
     path("", include("annonces.urls")),
 ]
