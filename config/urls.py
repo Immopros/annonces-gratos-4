95af7cd60954aca0509f22367bb89ff1af1a7631
Immopros <tip@immopros.fr>
2026-02-11 10:09:11 +0100

    Remove setup-admin URL path from urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from annonces.views import setup_admin
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("setup-admin/<str:token>/", setup_admin),
     path("", include("annonces.urls")),
 ]
