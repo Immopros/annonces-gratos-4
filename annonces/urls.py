4d00397f6a4bb0ae7e9cb58e861edfc77a2178d3
Immopros <tip@immopros.fr>
2026-02-11 15:08:21 +0100

    Fix URL patterns in annonces/urls.py

--- annonces/urls.py
+++ annonces/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import home, annonce_detail, annonce_create
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("annonces/nouvelle/", annonce_create, name="annonce_create"),
-    path("annonces/<int:pk>/", annonce_detail, name="annonce_detail"),
+    path('', home, name='home'),
+    path('annonces/<int:pk>/', annonce_detail, name='annonce_detail'),
+    path('annonces/nouvelle/', annonce_create, name='annonce_create'),
 ]
