660eda46c920b2409561a067e464bf1c2b7611f2
Immopros <tip@immopros.fr>
2026-02-12 00:39:35 +0100

    Update create_admin.py

--- annonces/management/commands/create_admin.py
+++ annonces/management/commands/create_admin.py
@@ -1,1 +1,30 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
 
+
+class Command(BaseCommand):
+   help = "Create initial superuser if not exists"
+
+   def handle(self, *args, **kwargs):
+       User = get_user_model()
+
+       username = os.environ.get("SUPERUSER_USERNAME")
+       email = os.environ.get("SUPERUSER_EMAIL")
+       password = os.environ.get("SUPERUSER_PASSWORD")
+
+       if not username or not email or not password:
+           self.stdout.write(self.style.ERROR("Missing SUPERUSER environment variables"))
+           return
+
+       if User.objects.filter(username=username).exists():
+           self.stdout.write("Superuser already exists")
+           return
+
+       User.objects.create_superuser(
+           username=username,
+           email=email,
+           password=password
+       )
+
+       self.stdout.write(self.style.SUCCESS("Superuser created successfully"))
